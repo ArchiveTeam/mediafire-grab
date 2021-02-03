840c371c43323087951977106f07b81e2f27e7fe
Thomas Glass <me@tom.glass>
2021-02-03 13:30:04 +0000

    Enable warrior + tracker host update

--- pipeline.py
+++ pipeline.py
@@ -40,7 +40,10 @@ WGET_AT = find_executable(
         'GNU Wget 1.20.3-at.20200919.01',
         'GNU Wget 1.20.3-at.20201030.01'
     ],
-    ['./wget-at']
+    [
+         './wget-at',
+         '/home/warrior/data/wget-at'
+    ]
 )
 
 if not WGET_AT:
@@ -52,10 +55,10 @@ if not WGET_AT:
 #
 # Update this each time you make a non-cosmetic change.
 # It will be added to the WARC files and reported to the tracker.
-VERSION = '20210114.01'
+VERSION = '20210203.01'
 USER_AGENT = 'Archive Team'
 TRACKER_ID = 'mediafire'
-TRACKER_HOST = 'trackerproxy.archiveteam.org'
+TRACKER_HOST = 'legacy-api.arpa.li'
 
 
 ###########################################################################
